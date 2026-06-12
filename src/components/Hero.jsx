dd0c2248d66c7c472db9c12f835d8f016b4e889f
Konan N'dassenan Loïc Harold Mehdy <loicmehdy11@outlook.fr>
2026-06-12 04:47:00 +0000

    Add 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ export default function Hero({ testerCount }) {
   <img src={logo} alt="GBORO" className="w-70 mb-4" />
    <p className="text-lg text-gray-300 mt-2 text-center mb-12">
     Le football ivoirien dans ta poche  
-    <h1 className="text-sm text-orange-400 font-medium">
+    {/* <h1 className="text-sm text-orange-400 font-medium">
   Bêta privée • Coupe du Monde 2026 Scores 
-</h1>
+</h1> */}
 <h1 className="mt-1 text-green-400 text-sm font-semibold">
 
   Déjà {testerCount} passionnés dans la liste bêta
