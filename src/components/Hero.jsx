8348a50d11bf0931fe685924e8f5b83955f0fe90
Konan N'dassenan Loïc Harold Mehdy <loicmehdy11@outlook.fr>
2026-06-12 04:48:48 +0000

    Add mobile beta CTA and improve beta form validation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,12 @@ export default function Hero({ testerCount }) {
   <img src={logo} alt="GBORO" className="w-70 mb-4" />
    <p className="text-lg text-gray-300 mt-2 text-center mb-12">
     Le football ivoirien dans ta poche  
-    {/* <h1 className="text-sm text-orange-400 font-medium">
+    <h1 className="text-sm text-orange-400 font-medium">
   Bêta privée • Coupe du Monde 2026 Scores 
-</h1> */}
-<h1 className="mt-1 text-green-400 text-sm font-semibold">
-
-  Déjà {testerCount} passionnés dans la liste bêta
-
 </h1>
-
+<h1 className="mt-1 text-green-400 text-sm font-semibold">
+  Déjà {testerCount} passionnés dans la liste bêta
+</h1>
     <a
   href="#beta"
   className="
@@ -22,7 +19,7 @@ export default function Hero({ testerCount }) {
     inline-flex
     items-center
     gap-2
-    mt-5
+    mt-1
     bg-green-500
     hover:bg-green-600
     px-6
